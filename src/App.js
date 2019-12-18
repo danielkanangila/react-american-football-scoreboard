0ca85465300f0ea39e7d7fecc5a015b2fa7d1c7a
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-12-18 00:26:58 -0500

    Created ActionButtons component for buttons section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 import Team from "./components/Team";
+import ActionButtons from "./components/ActionButtons";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -32,17 +33,7 @@ function App() {
         </div>
         <BottomRow />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={ handleScore } className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={ handleScore } className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={ handleScore } className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={ handleScore } className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-      </section>
+      <ActionButtons handleScore={handleScore} />
     </div>
   );
 }
